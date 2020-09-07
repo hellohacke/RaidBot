29e0af1a778283311e75a1461c91b7e5a1b038ae
hellohacke <70904024+hellohacke@users.noreply.github.com>
2020-09-07 16:44:13 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,3 +1,7 @@
-cls
+Start
 node index.js
-pause
+Bot Infos :
+Nom du bot    : Anti-Raid Bot#0188
+ID du bot     : 549193919917981706
+Token du bot : NTQ5MTkzOTE5OTE3OTgxNzA2.D2Vl8Q.i8NfTRDZDrC8aUoaxKTbaronnet
+Invitation : https://discordapp.com/oauth2/authorize?client_id=549193919917981706&scope=bot&permissions=2146958847
